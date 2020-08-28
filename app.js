0b0e5a99b5620c62bb0d746e33a28c6de9038ac4
cancalkan <cancalkaniletisim@gmail.com>
2020-08-28 03:10:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const reviews = [{
     id: 1,
     name: "savas bagcı ",
     job: "curtain store  owner",
-    img: "/img/savas-bagcı.jpg",
+    img: "img/savas-bagcı.jpg",
     text: "Putting together a website is a task, finding the right individual or company is even more complex. Thanks God I meet with him,not only he have great taste in design but he can guide you through the process and beyond ",
   link: BagcıPerdeLink
   },
@@ -14,7 +14,7 @@ const reviews = [{
     id: 2,
     name: "ahmet savas gokturk",
     job: "Business Solutions Executive",
-    img: "/img/ahmetsavasgokturk.jpg",
+    img: "img/ahmetsavasgokturk.jpg",
     text: "I have met Can for about 3 years, and we have worked with him on ERP and Business Intelligence within the scope of Industry-Academy cooperation projects. I saw him as a disciplined, hard-working, kind and hard-working person. I wish them to preserve these beautiful competencies for a lifetime and their continued success.",
     link: AhmetSavasGokturkLink
   },
@@ -22,7 +22,7 @@ const reviews = [{
     id: 3,
     name: "Yousra El Ouardy",
     job: "University Student",
-    img:"/img/Yusra.jpeg",
+    img:"img/Yusra.jpeg",
     text: "A perfectionist, he will try and try till making it perfect. He likes his major so much, he talks about it with a lot of passion. Before I didn't know much about computers but thanks to him I know some information now and I always ask him before downloading something for example. And also I practice English and Turkish  with him, and he helped me at making my LinkedIn account. He has great ideas.",
  link: Yousralink
   },
